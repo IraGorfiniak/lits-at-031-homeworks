31c24bfad7ff101b5ded8d6df94c31cdfb60d015
i.gorfiniak <i.gorfiniak@skelia.com>
2021-03-03 01:51:22 +0200

    Fixes for Homework 3 task 3

--- homework3/src/main/java/third/Third.java
+++ homework3/src/main/java/third/Third.java
@@ -5,9 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Third {
-    final static String filePath = "C:\\Users\\i.gorfiniak\\IdeaProjects\\lits-at-031-homeworks\\homework3\\src\\main\\java\\third\\Scientists";
-    final static String outputFilePath = "C:\\Users\\i.gorfiniak\\IdeaProjects\\lits-at-031-homeworks\\homework3\\src\\main\\java\\third\\FavouriteScientists";
-
+    final static String filePath = "homework3/src/main/java/third/Scientists";
+    final static String outputFilePath = "homework3/src/main/java/third/FavouriteScientists";
     private static HashMap<Integer, String> mapFile = new HashMap<>();
 
     public static void main(String[] args) {
@@ -18,16 +17,22 @@ public class Third {
         }
     }
 
+    private static boolean isPowerOfTwo(int number) {
+
+        return number > 0 && ((number & (number - 1)) == 0);
+
+    }
+
     private static Map<Integer, String> getHashMapFromTextFile() {
 
-        LineNumberReader lnr;
+        LineNumberReader readLinesFromFile;
         try {
             File scientists = new File(filePath);
-            lnr = new LineNumberReader(new FileReader(scientists));
+            readLinesFromFile = new LineNumberReader(new FileReader(scientists));
             String line;
             int i;
-            while ((line = lnr.readLine()) != null) {
-                i = lnr.getLineNumber();
+            while ((line = readLinesFromFile.readLine()) != null) {
+                i = readLinesFromFile.getLineNumber();
                 System.out.print(i + " ");
                 System.out.println(line);
                 mapFile.put(i, line);
@@ -42,27 +47,19 @@ public class Third {
 
     }
 
-    //new method
     public static void writeHashMapToTextFile(HashMap<Integer, String> mapNew) {
 
-        // create new BufferedWriter for the output file
         File outputFile = new File(outputFilePath);
         BufferedWriter bw = null;
 
         try {
             bw = new BufferedWriter(new FileWriter(outputFile));
 
-            // iterate map entries
             for (HashMap.Entry<Integer, String> entry :
                     mapNew.entrySet()) {
                 int number = entry.getKey();
-//                while (number != 1) {
-//                    if (number % 2 != 0) {
-//                       //return false;
-//                    }
-//                    number = number / 2;
-//                }
-                if (isPowerOfTwo(number)){
+
+                if (isPowerOfTwo(number)) {
                     bw.write(entry.getKey() + ":" + entry.getValue());
                     bw.newLine();
 
@@ -83,10 +80,6 @@ public class Third {
 
     }
 
-    private static boolean isPowerOfTwo(int number) {
 
-        return number > 0 && ((number & (number - 1)) == 0);
-
-    }
 }
 
