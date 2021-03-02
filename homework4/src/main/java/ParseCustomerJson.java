05231276661b96eef406b239a484bcb0d5449b17
i.gorfiniak <i.gorfiniak@skelia.com>
2021-03-03 01:36:19 +0200

    Homework 4, Updated Path

--- homework4/src/main/java/ParseCustomerJson.java
+++ homework4/src/main/java/ParseCustomerJson.java
@@ -7,7 +7,7 @@ public class ParseCustomerJson {
     public static void main (String[] args) {
         ObjectMapper mapper = new ObjectMapper();
         try{
-            Customer customer = mapper.readValue(new File("C:\\Users\\i.gorfiniak\\IdeaProjects\\lits-at-031-homeworks\\homework4\\src\\main\\java\\customer.json"), Customer.class);
+            Customer customer = mapper.readValue(new File("homework4/src/main/java/customer.json"), Customer.class);
             System.out.println(customer);
         }
         catch (IOException e){
